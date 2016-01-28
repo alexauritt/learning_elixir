3735dbfd156c3dfcd514496b25616c322c9d2c51
Alex Auritt <alexauritt@yahoo.com>
2016-01-28 11:25:33 -0500

    chap 7 work

--- lib/adding.ex
+++ lib/adding.ex
@@ -7,4 +7,12 @@ defmodule LearningElixir.Adding do
   def sum(n), do: n + sum(n-1)
   def gcd(x,0), do: x
   def gcd(x,y), do: gcd(y, rem(x,y))
+
+  def mapsum(list, f), do: sum(Enum.map(list, f))
+  def max([]), do: []
+  def max([head | []]), do: head
+  def max([head | tail]) do
+  	tail_max = max(tail)
+  	if tail_max > head, do: tail_max, else: head
+  end
 end
--- test/adding_test.exs
+++ test/adding_test.exs
@@ -15,4 +15,14 @@ defmodule LearningElixir.AddingTest do
   	assert LearningElixir.Adding.sum([3]) == 3
   	assert LearningElixir.Adding.sum([3,2,6,-4]) == 7
   end
+
+  test "mapsum" do
+    assert LearningElixir.Adding.mapsum([1,2,3], &(&1 * &1)) == 14
+  end
+
+  test "max" do
+    assert LearningElixir.Adding.max([]) == []
+    assert LearningElixir.Adding.max([5]) == 5
+    assert LearningElixir.Adding.max([5,2,8,6]) == 8
+  end
 end
